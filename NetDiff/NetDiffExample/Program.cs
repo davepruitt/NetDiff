4a0f36b94798557c76fe7e6661a5b8e5ab521ec7
David Pruitt <davepruitt@users.noreply.github.com>
2019-08-14 15:30:07 -0500

    Changing to .net standard

---
+++ NetDiff/NetDiffExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetDiffExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "hello";
+            string str2 = "heloworld!";
+            var result = NetDiff.DiffUtil.Diff(str1, str2);
+        }
+    }
+}
